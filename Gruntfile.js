c7b6132b6ae048aa511dd46f6cfe0333c3549486
Pavel Evsegneev <pavel@evsegneev.pp.ua>
2014-08-12 00:17:32 +0300

    Add grunt build command

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,49 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            options: {
+                stripBanners: true,
+                banner: '/*! \n<%= pkg.name %> - v<%= pkg.version %> - ' +
+                    '<%= grunt.template.today("yyyy-mm-dd") %> \n' +
+                    'Created by <%= pkg.author %>\n' +
+                    'License: <%= pkg.license %>\n' +
+                    'Visit <%= pkg.homepage %> for new versions or contributing. */\n'
+            },
+            dist: {
+                src: ['js/class.DOMWork.js'],
+                dest: 'dist/domwork.js'
+            }
+        },
+
+        clean: ["dist"],
+        /*copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['js/class.DOMWork.js'], dest: 'dist/',
+                        flatten: true,
+                        rename: function(dest, src) {
+                            return dest + 'domwork.js'
+                        }
+                    }
+                ]
+            }
+        },*/
+
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'dist/domwork.min.js': ['js/class.DOMWork.js']
+                }
+            }
+        },
+
         jshint: {
             all: [
                 "Gruntfile.js",
@@ -62,9 +105,14 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('test', ['jshint', 'jasmine:test']);
     grunt.registerTask('default', ['jshint','jasmine:coverage']);
+    grunt.registerTask('build', ['test', 'clean', 'concat', 'uglify']);
 
     grunt.registerTask('coverage', 'start web server for viewing istanbul coverage in browser', function() {
         grunt.task.run('jasmine:coverage');
--- README.md
+++ README.md
@@ -14,6 +14,8 @@ After that install bower dependencies: ```bower install```
 
 ###Grunt commands
 
+If you want to build production and minified version, use ```grunt build``` command
+
 You can use ```grunt test``` command to run jshint and jasmine tests.
 
 Also you can use ```grunt coverage``` command to view tests code coverage.
