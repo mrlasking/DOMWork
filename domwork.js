2301835abcb11062f6817d70ab18012919e81aba
Pavel Evsegneev <pavel@evsegneev.pp.ua>
2014-08-13 23:05:33 +0300

    Updates

--- domwork.js
+++ domwork.js
@@ -186,6 +186,77 @@ Visit https://github.com/mrlasking/domwork for new versions or contributing. */
 }(window || exports);
 !function(dw) {
 
+    /*istanbul ignore else*/
+    if (typeof Promise === 'undefined') {
+        window.Promise = RSVP.Promise;
+    }
+
+    dw().plugin({
+        name: "ajax",
+        func: function() {
+
+            var ajax = function(options, method) {
+
+                var reqString = '';
+                for (var d in options.data) {
+                    reqString += d + '=' + options.data.toString();
+                }
+
+                return new Promise(function(resolve, reject) {
+                    var req = new XMLHttpRequest();
+                    var method = method || 'GET';
+
+                    if (options.method && options.method.toUpperCase() === 'POST' ) {
+                        method = "POST";
+                    }
+
+                    var url = options.url + ((method == 'GET') ? "?"+reqString : "" );
+
+                    req.open(method, url);
+
+                    /*istanbul ignore next*/
+                    req.onload = function() {
+                        if (req.status == 200) {
+                            resolve(req.response);
+                        }
+                        else {
+                            reject(Error(req.statusText));
+                        }
+                    };
+
+                    /*istanbul ignore next*/
+                    req.onerror = function() {
+                        reject(Error("Network Error"));
+                    };
+
+                    if (method === "POST") {
+                        req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                    }
+
+                    req.send( reqString );
+
+                });
+
+            };
+
+            dw.ajax = dw.ajax || ajax;
+            dw.get = dw.get || function(options) {
+                return ajax(options, 'GET');
+            };
+            dw.post = dw.post || function(options) {
+                return ajax(options, 'POST');
+            };
+
+            return "Use it from $DW directly: $DW.ajax, $DW.get, $DW.post"
+
+        },
+
+        needToRun: true
+    });
+
+}($DW);
+!function(dw) {
+
   dw().plugin({
     name: "handlebars",
     func: function() {
@@ -2946,151 +3017,6 @@ Visit https://github.com/mrlasking/domwork for new versions or contributing. */
 
     needToRun: true
   });
-
-}($DW);
-!function(dw) {
-
-	dw().plugin({
-		name: "model",
-		func: function() {
-
-			var model = function() {
-
-				return function( data ) {
-
-					var properties = {};
-					var m = data;
-
-					var result = {};
-
-					for (var n in data) {
-						result[n] = modelValue(data, n);
-					}
-
-					function modelValue(data, n) {
-
-						var bindedTo = [];
-
-						return (function(data, n) {
-
-							var f = function(value) {
-
-								if (typeof value !== 'undefined') {
-
-									if (value !== f.toString()) {
-
-										updateBinded(value);
-
-										f.toString = function() {
-											return value;
-										}
-									}
-
-									
-								}
-								
-								return f.toString();
-							}
-
-							f.toString = function() {
-								return data[n];
-							}
-
-							f.bindTo = function( dwnodes ) {
-
-								dwnodes.elements.forEach(function(item) {
-									if (item.nodeName === 'INPUT') {
-										item.value = f.toString();
-										bindedTo.push(item);
-
-										!function(item) {
-											dw().node(item).bind('keydown', function() {
-												setTimeout(function() {
-													f.toString = function() {
-														return item.value;
-													}
-													
-													updateBinded(item.value, item);
-
-												}, 0);
-											});
-										}(item);
-										return;
-									} 
-									if (item.nodeName === 'DIV') {
-										item.innerText = f.toString();
-										bindedTo.push(item);
-										return;
-									}
-									if (item.nodeName === 'TEXTAREA') {
-										item.value = f.toString();
-										bindedTo.push(item);
-
-										!function(item) {
-											dw().node(item).bind('keydown', function() {
-												setTimeout(function() {
-													f.toString = function() {
-														return item.value;
-													}
-
-													updateBinded(item.value, item);
-
-												}, 0);
-											});
-										}(item);
-										return;
-									} 
-								});
-
-							}
-
-							function updateBinded(value, item) {
-
-								var _update = function() {
-									if (bindedTo[i].nodeName === 'INPUT') {
-										bindedTo[i].value = value;
-									} 
-									if (bindedTo[i].nodeName === 'TEXTAREA') {
-										bindedTo[i].value = value;
-									}
-									if (bindedTo[i].nodeName === 'DIV') {
-										bindedTo[i].innerText = value;
-									}
-								}
-
-								for (var i in bindedTo) {
-									if ( typeof item !== 'undefined') {
-										if (item != bindedTo[i]) {
-											_update();
-										}
-									} else {
-										_update();
-									}
-								}
-
-							}
-
-							properties[data[n]] = f;
-
-							return f;
-							
-						})(data, n);
-					}
-
-					return result;
-
-				}
-
-			}
-
-			dw.Model = dw.Model || new model();
-
-			return "Use it from $DW directly: $DW.Model()";
-
-		},
-
-		needToRun: true
-	});
 
 }($DW);
 !function(dw) {
